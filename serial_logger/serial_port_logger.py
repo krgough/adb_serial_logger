6e30f00da358217d4cf584a1703fd0683ed78f63
krgough <krgough@gmail.com>
2022-01-05 17:12:38 +0000

    Black formatting changes

--- serial_logger/serial_port_logger.py
+++ serial_logger/serial_port_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''
+"""
 @author: Keith Gough
 
 Serial Port Logger - for logging serial port activity on a device
@@ -10,7 +10,7 @@ been received on the same serial port e.g. device sends a particular
 debug message and then we send a command to the device to trigger
 some wanted behaviour.
 
-'''
+"""
 
 
 import sys
@@ -40,8 +40,8 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 LOGGER_HANDLER = logging.StreamHandler()  # This logs to stderr
 LOGGER_FMT = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    "%Y-%m-%d %H:%M:%S")
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S"
+)
 LOGGER_HANDLER.setFormatter(LOGGER_FMT)
 LOGGER.addHandler(LOGGER_HANDLER)
 # logging.basicConfig(level=logging.DEBUG)
@@ -52,40 +52,36 @@ LOGGER_FILE.setLevel(logging.INFO)
 
 # Create and add a handler to the logger
 HANDLER = handlers.RotatingFileHandler(
-    cfg.LOG_FILENAME,
-    maxBytes=cfg.LOG_MAX_BYTES,
-    mode='a',
-    backupCount=cfg.LOG_COUNT)
-FILE_FMT = logging.Formatter('%(asctime)s,%(message)s', "%Y-%m-%d %H:%M:%S")
+    cfg.LOG_FILENAME, maxBytes=cfg.LOG_MAX_BYTES, mode="a", backupCount=cfg.LOG_COUNT
+)
+FILE_FMT = logging.Formatter("%(asctime)s,%(message)s", "%Y-%m-%d %H:%M:%S")
 HANDLER.setFormatter(FILE_FMT)
 LOGGER_FILE.addHandler(HANDLER)
 
 
 def time_string():
-    """ Returns a formatted time string
-    """
+    """Returns a formatted time string"""
     return datetime.datetime.now().strftime(DATE_FORMAT)
 
 
 # Serial Thread Methods
 def open_serial_port(port, baud):
-    """ Open the given serial port
-    """
+    """Open the given serial port"""
     try:
         ser = serial.Serial(port, baud, timeout=1)
         LOGGER.info("Serial port opened...%s", port)
     except IOError as err:
-        LOGGER.error('Error opening port: %s', err)
+        LOGGER.error("Error opening port: %s", err)
         return None
     return ser
 
 
 def start_serial_threads(ser):
-    """ Starts the wanted serial threads
+    """Starts the wanted serial threads
 
-        Stop any currently running read/write threads.
-        Start the wanted ones.
-        Interact with read/write is done via message queues rxQueue and txQueue
+    Stop any currently running read/write threads.
+    Start the wanted ones.
+    Interact with read/write is done via message queues rxQueue and txQueue
     """
     # Turn off any current threads
     stop_threads()
@@ -97,29 +93,27 @@ def start_serial_threads(ser):
     read_thread = threading.Thread(target=serial_read_handler, args=(ser,))
     read_thread.daemon = True  # This kills the thread when main program exits
     read_thread.start()
-    read_thread.name = 'read_thread'
+    read_thread.name = "read_thread"
     THREAD_POOL.append(read_thread)
 
 
 def stop_threads():
-    """ Set the stop event and wait for all threads to exit
-    """
+    """Set the stop event and wait for all threads to exit"""
     STOP_THREAD.set()
     for thd in THREAD_POOL:
         thd.join()
 
 
 def serial_read_handler(ser):
-    """ Serial port read thread handler
-    """
-    LOGGER.info('Serial readThread start')
+    """Serial port read thread handler"""
+    LOGGER.info("Serial readThread start")
     while not STOP_THREAD.is_set():
         try:
             reading = ser.readline().strip().decode()
         except UnicodeDecodeError:
-            reading = ''
+            reading = ""
 
-        if reading != '':
+        if reading != "":
             # Make sure Qs are not full and blocking
             if RX_QUEUE.full():
                 LOGGER.error("RxQueue is full.  Dumping oldest reading")
@@ -128,12 +122,11 @@ def serial_read_handler(ser):
             RX_QUEUE.put(reading)
             LOGGER.debug(reading)
 
-    LOGGER.info('Serial readThread exit')
+    LOGGER.info("Serial readThread exit")
 
 
 def start_serial_logger():
-    """  Start the logger
-    """
+    """Start the logger"""
     # Open the serial port
     ser = open_serial_port(cfg.PORT, cfg.BAUD)
     if not ser:
@@ -150,12 +143,12 @@ def start_serial_logger():
         time.sleep(0.1)  # Sleep to let other processes run
 
     # Shutdown
-    LOGGER.info('Run flag is missing.  Shutting down.')
+    LOGGER.info("Run flag is missing.  Shutting down.")
     stop_threads()
     ser.close()
     logging.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_serial_logger()
-    print('All done.')
+    print("All done.")
